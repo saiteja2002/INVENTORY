56a07c8659e604f4ab422afa4e6618ce3c2470da
saiteja2002 <65535824+saiteja2002@users.noreply.github.com>
2022-08-22 23:27:20 +0530

    Update run.py

--- Neural_Architeture_Search_CNN/run.py
+++ Neural_Architeture_Search_CNN/run.py
@@ -3,16 +3,8 @@ import numpy as np
 from basemodel_architeture_search import basemodel_architeture_search
 from keras_preprocessing.image import ImageDataGenerator
 from SampleSpaceGenerate_CreateandTrainModel import Generate_Sample_Space_and_Create_and_train_model
-def top_givenn_architeture(n):
-    data_file = 'model_data_generated_sofar.pkl'
-    with open(data_file, 'rb') as f:
-        data = pickle.load(f)
-    val_accs = [item[1] for item in data]
-    sorted_idx = np.argsort(val_accs)[::-1]
-    data = [data[x] for x in sorted_idx]
-    search_space = Generate_Sample_Space_and_Create_and_train_model()
-    for seq_data in data[:n]:
-        print('Architecture', search_space.id_to_config(seq_data[0]),"        ",'Validation Accuracy:', seq_data[1])
+
+
 # All images will be rescaled by 1./255
 
 train_datagen = ImageDataGenerator(rescale=1/255)
@@ -37,4 +29,13 @@ main = basemodel_architeture_search(train_generator, validation_generator)
 #running the search
 data = main.search()
 #getting the top 5 architetures for this data
-top_givenn_architeture(6)
+n=6
+data_file = 'model_data_generated_sofar.pkl'
+with open(data_file, 'rb') as f:
+    data = pickle.load(f)
+val_accs = [item[1] for item in data]
+sorted_idx = np.argsort(val_accs)[::-1]
+data = [data[x] for x in sorted_idx]
+search_space = Generate_Sample_Space_and_Create_and_train_model()
+for seq_data in data[:n]:
+    print('Architecture', search_space.id_to_config(seq_data[0]),"        ",'Validation Accuracy:', seq_data[1])
