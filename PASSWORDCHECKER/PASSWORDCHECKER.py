6928f5243e6627b1e896a05123176d3c4147d90e
saiteja2002 <65535824+saiteja2002@users.noreply.github.com>
2022-08-23 19:06:05 +0530

    Update PASSWORDCHECKER.py

--- PASSWORDCHECKER/PASSWORDCHECKER.py
+++ PASSWORDCHECKER/PASSWORDCHECKER.py
@@ -1,41 +1,48 @@
-import requests
 import hashlib
+import requests
 import sys
 
-#requests to api with first 5 characters of password encoded in sha1 and recieves all the sha1 encoded passwords
-#which have the same first 5 characters
 
-def request_api_data(query_char):
-  url = 'https://api.pwnedpasswords.com/range/' + query_char
-  res = requests.get(url)
-  if res.status_code != 200:
-    raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
-  return res
+def request_api(hash_first_5):
+    #sends the request to api
+    url = 'https://api.pwnedpasswords.com/range/' + hash_first_5
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Error not working")
+        raise RuntimeError(f'Error in API fetching, status {response.status_code}. Check the API and try again')
+    return response
 
 
-# returns number of times the given passwords have been been hacked
-def get_password_leaks_count(hashes, hash_to_check):
-  hashes = (line.split(':') for line in hashes.text.splitlines())
-  for h, count in hashes:
-    if h == hash_to_check:
-      return count
-  return 0
-# encodes the given password with sha1 and calls the above functions
-def check(password):
-  sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-  first5_char, tail = sha1password[:5], sha1password[5:]
-  response = request_api_data(first5_char)
-  return get_password_leaks_count(response, tail)
-#displays the count
-def completed(args):
-  for password in args:
-    count = check(password)
-    if count:
-      print(f'{password} was found {count} times... change your password!')
+def database_password_count(response_data, reaming_5):
+    api_response = (line.split(':') for line in response_data.splitlines())
+    for h, count in api_response:
+        if h == reaming_5:
+            return int(count)
+    return 0
+
+
+def number_of_times_found_in_database(password):
+    sha1_encoding = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()#hahses the given password using sha1hashing
+    first5chars, remainig_5 = sha1_encoding[:5], sha1_encoding[5:]#splits into first5 and last 5
+    response = request_api(first5chars)#sends a request to api using first 5 characters of the hashed password
+    count = database_password_count(response.text, remainig_5)#finds the number of times the password was in the reponse
+    return count
+
+
+def main(given_passwords):
+    for password in given_passwords:
+        count = number_of_times_found_in_database(password)
+        if count:
+            print(f'{password} was found {count} times in databse. You should probably change your password')
+        else:
+            print(f'{password} was not found in the dataBase. No need to change ')
+    return 'Done'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        # Use sys.exit() only to display return message from the main()
+        # function at end of work
+        sys.exit(main(sys.argv[1:]))
     else:
-      print(f'{password} was NOT found. You are safe!')
-  return 'done!'
-
-
-sys.exit(completed(sys.argv[1:]))
-
+        pass
